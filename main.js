de50998288b5541368e563c4494675f3a36234ae
meflems <89581469+meflems@users.noreply.github.com>
2022-03-10 19:41:27 +0700

    update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ if (level === 1) {
 		xp = 0;
 		console.log(level);
 		console.log(xp);
-		Swal.fire('Level Up to level 2')
+		document.getElementById("levelUp").innerHTML = "congratulations, level up to level 2";
 	}
 }
 
