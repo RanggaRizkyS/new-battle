853daad2d4663d68afc9976dc45fc5a9408bb927
meflems <89581469+meflems@users.noreply.github.com>
2022-03-10 21:29:17 +0700

    update level.js

--- level.js
+++ level.js
@@ -1,7 +1,8 @@
-var level = 1;
-var xp = 0;
+
 var hp_player = 1000;
 var hp_enemy = 1000;
+var level = 1;
+var xp = 0;
 let attack = Math.floor(Math.random() * 100) + 349;
 let heal = Math.floor(Math.random() * 100) + 240;
 
