01f9bee9d67fa432f0bb148f4f94459806705246
meflems <89581469+meflems@users.noreply.github.com>
2022-03-10 20:30:33 +0700

    update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var hp_player = 1000;
 var level = 1;
 var hp_enemy = 1000;
-var xp = 700;
+var xp = 0;
 let attack = Math.floor(Math.random() * 100) + 349;
 let heal = Math.floor(Math.random() * 100) + 240;
 
@@ -21,5 +21,90 @@ if (level === 2) {
 		xp = 0;
 		console.log(level);
 		console.log(xp);
+		document.getElementById("levelUp").innerHTML = "Congratulations, Succes level up to level 3";
 	}
+}
+
+if (level === 3) {
+	if (xp > 3000) {
+		level++;
+		xp = 0;
+		console.log(level);
+		console.log(xp);
+		document.getElementById("levelUp").innerHTML = "Congratulations, Succes level up to level 2";
+	}
+}
+
+if (level === 4) {
+	if (xp > 4000) {
+		level++;
+		xp = 0;
+		console.log(level);
+		console.log(xp);
+		document.getElementById("levelUp").innerHTML = "Congratulations, Succes level up to level 2";
+	}
+}
+
+if (level === 5) {
+	if (xp > 5000) {
+		level++;
+		xp = 0;
+		console.log(level);
+		console.log(xp);
+		document.getElementById("levelUp").innerHTML = "Congratulations, Succes level up to level 2";
+	}
+}
+
+if (level === 6) {
+	if (xp > 5700) {
+		level++;
+		xp = 0;
+		console.log(level);
+		console.log(xp);
+		document.getElementById("levelUp").innerHTML = "Congratulations, Succes level up to level 2";
+	}
+}
+
+if (level === 7) {
+	if (xp > 6000) {
+		level++;
+		xp = 0;
+		console.log(level);
+		console.log(xp);
+		document.getElementById("levelUp").innerHTML = "Congratulations, Succes level up to level 2";
+	}
+}
+
+if (level === 8) {
+	if (xp > 7000) {
+		level++;
+		xp = 0;
+		console.log(level);
+		console.log(xp);
+		document.getElementById("levelUp").innerHTML = "Congratulations, Succes level up to level 2";
+	}
+}
+
+if (level === 9) {
+	if (xp > 9000) {
+		level++;
+		xp = 0;
+		console.log(level);
+		console.log(xp);
+		document.getElementById("levelUp").innerHTML = "Congratulations, Succes level up to level 2";
+	}
+}
+
+if (level === 10) {
+	if (xp > 9500) {
+		level++;
+		xp = 0;
+		console.log(level);
+		console.log(xp);
+		document.getElementById("levelUp").innerHTML = "Congratulations, Succes level up to level 2";
+	}
+}
+
+if (level === 11) {
+	document.getElementById("levelUp").innerHTML = "level max";
 }
