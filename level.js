3bfdba8e2019e7acfa9e89b954976f63d3070ae9
meflems <89581469+meflems@users.noreply.github.com>
2022-03-10 21:38:13 +0700

    update level.js

--- level.js
+++ level.js
@@ -3,13 +3,17 @@ var hp_player = 1000;
 var hp_enemy = 1000;
 var level = 1;
 var xp = 0;
-var attack = Math.floor(Math.random() * 100) + 349;
-var heal = Math.floor(Math.random() * 100) + 240;
+var attack = Math.floor(Math.random() * 25) + 120;
+var heal = Math.floor(Math.random() * 10) + 70;
+let enemyAttack = Math.floor(Math.random() * 20) + 120;
 
 function buttonAttack() {
 	hp_enemy -= attack;
 	var succes = hp_enemy -= attack;
 	document.getElementById("result").innerHTML = "you hit enemy with"+attack+" damage, now enemy have"+succes+"healt";
+	hp_player -= enemyAttack;
+	var failed = hp_player -= enemyAttack;
+	document.getElementById("result").innerHTML = "enemy hit you with"+enemyAttack+" damage, now you have"+failed+"healt";
 }
 
 if (level === 1) {
